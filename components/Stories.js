2726458b6ea43f01c8cabc42df687c5b0c7798b4
captainlevi0220 <captainlevi0220@protonmail.com>
2022-03-29 20:57:25 -0400

    setting up Storys

--- components/Stories.js
+++ components/Stories.js
@@ -1,20 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { faker } from '@faker-js/faker'
+import Story from './Story'
 
 function Stories() {
+  const [suggestions, setSuggestions] = useState([])
+
   useEffect(() => {
     const suggestions = [...Array(20)].map((_, i) => ({
       ...faker.helpers.contextualCard(),
       id: i,
     }))
 
-    console.log(suggestions)
+    setSuggestions(suggestions)
   }, [])
   return (
     <div>
-      {/* Story  */}
-      {/* Story  */}
-      {/* Story  */}
+      {suggestions.map((profile) => (
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
+      ))}
     </div>
   )
 }
